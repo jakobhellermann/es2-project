e44f93450f51ac7585ad4ccbefd943681ce213c6
Jakob Hellermann <jakob.hellermann@protonmail.com>
2024-08-15 17:56:08 +0200

    backend: format code

--- backend/main.py
+++ backend/main.py
@@ -8,9 +8,10 @@ import scipy
 
 app = Flask(__name__)
 
+
 @app.route("/transcribe", methods=["POST"])
 def hello_world():
-    tmp_file  = tempfile.NamedTemporaryFile()
+    tmp_file = tempfile.NamedTemporaryFile()
 
     try:
         print("writing audio data to temp file:", tmp_file.name)
@@ -26,34 +27,43 @@ def hello_world():
 
         print("transcription result:", result["text"])
 
-        system_prompt = "Du bist ein hilfreicher Assistent. Bitte halte dich kurz und prägnant."
+        system_prompt = (
+            "Du bist ein hilfreicher Assistent. Bitte halte dich kurz und prägnant."
+        )
 
         prompt = f"{system_prompt} USER: {result["text"]} ASSISTANT: "
         print("prompt:", prompt)
 
         output = llm(
             prompt,
-            max_tokens=100, # Generate up to 32 tokens, set to None to generate up to the end of the context window
-            stop=["USER:", "\n"], # Stop generating just before the model would generate a new question
-            echo=True # Echo the prompt back in the output
+            max_tokens=100,  # Generate up to 32 tokens, set to None to generate up to the end of the context window
+            stop=[
+                "USER:",
+                "\n",
+            ],  # Stop generating just before the model would generate a new question
+            echo=True,  # Echo the prompt back in the output
         )
 
         text = output["choices"][0]["text"]
-        text = text[len(prompt):].strip()
+        text = text[len(prompt) :].strip()
 
-        out_tmp_file  = tempfile.NamedTemporaryFile()
-
+        out_tmp_file = tempfile.NamedTemporaryFile()
 
         inputs = tts_tokenizer(text, return_tensors="pt")
         with torch.no_grad():
             output = tts_model(**inputs).waveform
-            scipy.io.wavfile.write(out_tmp_file.name, rate=tts_model.config.sampling_rate, data=output.cpu().float().numpy().transpose())
+            scipy.io.wavfile.write(
+                out_tmp_file.name,
+                rate=tts_model.config.sampling_rate,
+                data=output.cpu().float().numpy().transpose(),
+            )
 
         with open(out_tmp_file.name, "rb") as f:
             return f.read()
     except Exception as e:
         print(e)
         return "transcription failed", 500
+
 
 def main():
     global model
@@ -62,14 +72,15 @@ def main():
 
     global llm
     llm = Llama(
-      model_path="./em_german_leo_mistral.Q4_K_M.gguf",
+        model_path="./em_german_leo_mistral.Q4_K_M.gguf",
     )
 
     global tts_model, tts_tokenizer
     tts_model = VitsModel.from_pretrained("facebook/mms-tts-deu")
     tts_tokenizer = AutoTokenizer.from_pretrained("facebook/mms-tts-deu")
 
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, host="0.0.0.0", port=8080)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
