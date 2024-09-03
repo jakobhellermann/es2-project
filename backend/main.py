31754a7ef8db3a7eb0dc330080a30bb3404252ac
Jakob Hellermann <jakob.hellermann@protonmail.com>
2024-09-03 18:24:21 +0200

    backend: make models configurable

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,5 @@
-from flask import Flask, Request, request, send_from_directory
-from flask_cors import CORS, cross_origin
-import whisper
+from flask import Flask, request, send_from_directory
+from flask_cors import CORS
 from llama_cpp import Llama
 import tempfile
 from transformers import VitsModel, AutoTokenizer, pipeline
@@ -31,13 +30,13 @@ class SpeechToTextModel:
         return transcription["text"].strip()
 
 
-class LargeLanguageModel:
-    def __init__(self):
+class LlamaModel:
+    def __init__(self, model_path):
         self.model = Llama(
-            model_path="./models/llm/em_german_leo_mistral.Q4_K_M.gguf",
+            model_path=model_path,
         )
 
-    def eval(self, system_propmt: str, user_prompt: str) -> str:
+    def eval(self, system_prompt: str, user_prompt: str) -> str:
         prompt = f"{system_prompt} USER: {user_prompt} ASSISTANT: "
 
         output = self.model(
@@ -73,9 +72,18 @@ class TextToSpeechModel:
 
 
 start = time.time()
-stt_model = SpeechToTextModel()
-llm_model = LargeLanguageModel()
-tts_model = TextToSpeechModel()
+llm_models = {
+    "em_german_leo_mistral-Q4": LlamaModel(
+        "./models/llm/em_german_leo_mistral.Q4_K_M.gguf"
+    ),
+    "llama2-7b-Q4": LlamaModel("./models/llm/llama-2-7b.Q4_K_M.gguf"),
+}
+stt_models = {
+    "whisper-base": SpeechToTextModel(),
+}
+tts_models = {
+    "facebook_mms-deu": TextToSpeechModel(),
+}
 end = time.time()
 
 print(f"Load time: {end-start}")
@@ -94,10 +102,10 @@ def time_function(f):
     return result, end - start
 
 
-def write_audio_to_disk(audio):
+def write_audio_to_disk(sampling_rate, audio):
     id = uuid.uuid4()
     out_file = f"data/{id}.wav"
-    scipy.io.wavfile.write(out_file, rate=tts_model.sampling_rate, data=audio)
+    scipy.io.wavfile.write(out_file, rate=sampling_rate, data=audio)
     return f"{base_url}/audio/{id}.wav"
 
 
@@ -106,12 +114,33 @@ def serve_audio_files(path):
     return send_from_directory("data", path)
 
 
+@app.route("/models")
+def available_models():
+    return {
+        "stt": list(stt_models.keys()),
+        "llm": list(llm_models.keys()),
+        "tts": list(tts_models.keys()),
+    }
+
+
 @app.route("/assistant/text", methods=["POST"])
 def assistant_text():
-    req = request.json
-    if not req or "text" not in req:
+    request_body = request.json
+    if not request_body or "text" not in request_body:
         raise Exception("400")
-    user_text = req["text"]
+    user_text = request_body["text"]
+
+    llm_model_name = request.args.get("llm_model", next(iter(llm_models)))
+    if llm_model_name not in llm_models:
+        raise Exception(f"LLM model '{llm_model_name}' does not exist")
+    llm_model = llm_models[llm_model_name]
+
+    tts_model_name = request.args.get("tts_model", next(iter(tts_models)))
+    if tts_model_name not in tts_models:
+        raise Exception(f"tts model '{tts_model_name}' does not exist")
+    tts_model = tts_models[tts_model_name]
+
+    print(f"Using models llm={llm_model_name} tts={tts_model_name}")
 
     result_text, time_llm = time_function(
         lambda: llm_model.eval(system_prompt, user_text)
@@ -120,10 +149,11 @@ def assistant_text():
         lambda: tts_model.text_to_speech(result_text)
     )
 
-    output_url = write_audio_to_disk(output_audio)
+    output_url = write_audio_to_disk(tts_model.sampling_rate, output_audio)
 
     return {
         "timings": {
+            "time_stt": 0,
             "time_llm": time_llm,
             "time_tts": time_tts,
         },
@@ -137,6 +167,26 @@ def assistant_text():
 @app.route("/assistant/audio", methods=["POST"])
 def assistant():
     input_audio = request.get_data()
+
+    tts_model_name = request.args.get("tts_model", next(iter(tts_models)))
+    if tts_model_name not in tts_models:
+        raise Exception(f"tts model '{tts_model_name}' does not exist")
+    tts_model = tts_models[tts_model_name]
+
+    llm_model_name = request.args.get("llm_model", next(iter(llm_models)))
+    if llm_model_name not in llm_models:
+        raise Exception(f"LLM model '{llm_model_name}' does not exist")
+    llm_model = llm_models[llm_model_name]
+
+    stt_model_name = request.args.get("stt_model", next(iter(stt_models)))
+    if stt_model_name not in stt_models:
+        raise Exception(f"stt model '{stt_model_name}' does not exist")
+    stt_model = stt_models[stt_model_name]
+
+    print(
+        f"Using models stt={stt_model_name} llm={llm_model_name} tts={tts_model_name}"
+    )
+
     transcription, time_stt = time_function(
         lambda: stt_model.speech_to_text(input_audio)
     )
@@ -147,7 +197,7 @@ def assistant():
         lambda: tts_model.text_to_speech(result_text)
     )
 
-    output_url = write_audio_to_disk(output_audio)
+    output_url = write_audio_to_disk(tts_model.sampling_rate, output_audio)
 
     return {
         "timings": {
--- backend/models/download_models.sh
+++ backend/models/download_models.sh
@@ -7,6 +7,8 @@ huggingface-cli download facebook/wav2vec2-base-960h --local-dir stt/wave2vec2
 
 # https://huggingface.co/TheBloke/em_german_leo_mistral-GGUF
 huggingface-cli download TheBloke/em_german_leo_mistral-GGUF em_german_leo_mistral.Q4_K_M.gguf --local-dir llm/
+# https://huggingface.co/TheBloke/Llama-2-7B-GGUF
+huggingface-cli download TheBloke/Llama-2-7B-GGUF llama-2-7b.Q4_K_M.gguf --local-dir llm/
 
 # https://huggingface.co/facebook/mms-tts-deu
 huggingface-cli download facebook/mms-tts-deu --local-dir tts/facebook_mms-tts-deu
