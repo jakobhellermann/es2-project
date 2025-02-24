7a7595e9f01ff69519ea4562aa5107df752f0308
Gordon Kirsch <therealfantasia@gmx.de>
2025-02-24 17:11:22 +0100

    tablet styling

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import {Observable, ReplaySubject} from "rxjs";
 import {ReactiveFormsModule} from "@angular/forms";
-import {AsyncPipe, NgClass, NgForOf, NgIf, NgSwitch, NgSwitchCase} from "@angular/common";
-import {NgbDropdown, NgbDropdownItem, NgbDropdownMenu, NgbDropdownToggle, NgbTooltip} from "@ng-bootstrap/ng-bootstrap";
+import {AsyncPipe, NgSwitch, NgSwitchCase} from "@angular/common";
 import {NgxBootstrapIconsModule} from "ngx-bootstrap-icons";
 import {MenuComponent} from "../components/menu/menu.component";
 import {GridComponent} from "../grid/grid.component";
-import {ChatComponent} from "../components/chat/chat.component";
 import {SingleComponent} from "../single/single.component";
 
 @Component({
@@ -15,18 +13,9 @@ import {SingleComponent} from "../single/single.component";
   imports: [
     ReactiveFormsModule,
     AsyncPipe,
-    NgForOf,
-    NgClass,
-    NgIf,
-    NgbTooltip,
-    NgbDropdown,
-    NgbDropdownToggle,
-    NgbDropdownMenu,
-    NgbDropdownItem,
     NgxBootstrapIconsModule,
     MenuComponent,
     GridComponent,
-    ChatComponent,
     SingleComponent,
     NgSwitchCase,
     NgSwitch
@@ -42,27 +31,6 @@ export class HomeComponent {
     this.viewMode$.next('single')
     this._viewMode = this.viewMode$.asObservable()
   }
-
-  /*test() {
-    const text = this.textInput.value;
-
-    this.insertRequest(text);
-
-    this.messages.push({
-      message: '',
-      reply: true,
-      telemetry: null,
-      loading: false
-    });
-
-    this.messages$.next(this.messages)
-
-    this.botService.test(text, (res) => {
-      const lastMessage = this.messages[this.messages.length - 1];
-
-      lastMessage.message += res;
-    })
-  }*/
 
   onViewMode(mode: string) {
     this.viewMode$.next(mode)
